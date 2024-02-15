86f977879e41ade49108034d7dda11384a8f956a
Minho Yoo <ymh0951@naver.com>
2024-02-15 22:26:04 +0900

    제네릭

--- src/a.ts
+++ src/a.ts
@@ -1,18 +1,3 @@
-// interface Person {
-//     name: string;
-//     age: number;
-// }
-
-// interface Developer {
-//     name: string;
-//     age: string;
-// }
-
-// interface Admin {
-//     name: string;
-//     age: boolean;
-// }
-
 interface Anyone<T> {
     name: string;
     age: T
